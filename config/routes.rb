4d49d0954917cf6cd088aa8262caa3c61602c689
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-10-13 22:56:45 +0900

    背景画像等の設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create, :show]
   resources :sessions
-  get '*path', to: 'application#error_404'
   root to: 'users#new'
+  get '*path', to: 'application#error_404'
 end
